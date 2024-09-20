dce6e37a2e64c7a7a5cbf28b7bca696dd13855df
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2024-09-20 20:25:59 +0530

    solve

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT ||3000;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
